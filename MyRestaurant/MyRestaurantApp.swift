7579b9950e95b10c686fc6d7449dec31e8652eaa
vinayakbora <vinayakbora09@gmail.com>
2024-04-01 11:18:39 +0530

    Initial Commit

---
+++ MyRestaurant/MyRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRestaurantApp.swift
+//  MyRestaurant
+//
+//  Created by Vinayak Bora on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MyRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
